86f2fbcc164b8fdb6accfd5de18ea1e9b8bedc35
Luke Policinski <luke@lukepolo.com>
2018-03-28 10:07:19 -0500

    removal of more things

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -1,11 +1,5 @@
 @extends('layouts.public')
 
-
-@push('styles')
-    <link rel="stylesheet" crossorigin href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.26.0/codemirror.css">
-    <link rel="stylesheet" crossorigin href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.26.0/theme/neat.css">
-@endpush
-
 @section('content')
 
     @include('layouts.core.github')
@@ -20,14 +14,12 @@
                 Spend your time writing actual working code instead of cleaning the old ones
             </h2>
 
-            <a href="{{ action('Auth\OauthController@link', 'github') }}"
-               class="margin-top-3 s-button s-button-blue s-button-fat">
+            <a href="{{ action('Auth\OauthController@link', 'github') }}" class="margin-top-3 s-button s-button-blue s-button-fat">
                 Create an account
             </a>
         </div>
     </div>
 
     @include('landing.partials.features')
-    @include('landing.partials.playground')
 
 @endsection
