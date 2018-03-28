50993f069231b4af0c33ce76c287b0602a4ef82e
Luke Policinski <luke@lukepolo.com>
2018-03-28 08:26:53 -0500

    varie update version

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.public')
 
+
 @push('styles')
     <link rel="stylesheet" crossorigin href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.26.0/codemirror.css">
     <link rel="stylesheet" crossorigin href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.26.0/theme/neat.css">
 @endpush
 
 @section('content')
+
+    @include('layouts.core.github')
+
     <div class="relative">
         <div class="landing-section-1">
             <h1 class="zoomIn animated">
@@ -16,7 +20,8 @@
                 Spend your time writing actual working code instead of cleaning the old ones
             </h2>
 
-            <a href="{{ action('Auth\OauthController@link', 'github') }}" class="margin-top-3 s-button s-button-blue s-button-fat">
+            <a href="{{ action('Auth\OauthController@link', 'github') }}"
+               class="margin-top-3 s-button s-button-blue s-button-fat">
                 Create an account
             </a>
         </div>
